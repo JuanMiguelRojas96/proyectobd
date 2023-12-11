391f901a7608747a87c29f2369ce5a0e2b06476e
JuanMiguel <juan.noriega@correounivalle.edu.co>
2023-12-11 13:38:46 -0500

    Se añade la card que indica la ciudad

---
+++ client/src/app/components/card-city/card-city.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-card-city',
+  templateUrl: './card-city.component.html',
+  styleUrl: './card-city.component.css'
+})
+export class CardCityComponent {
+
+}
