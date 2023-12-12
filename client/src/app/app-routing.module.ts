4af0651db8b6fbb578ea7a20aabbe91c9afb68b6
JuanMiguel <juan.noriega@correounivalle.edu.co>
2023-12-12 00:17:22 -0500

    Se agrega el componente Login y sus verificaciones de campos, tambien se añaden las rutas para el logo de Cinefilos y para iniciar sesion

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CardCityComponent } from './components/card-city/card-city.component';
+import { LoginComponent } from './components/login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'home', component:CardCityComponent},
+  {path: 'login', component:LoginComponent},
+  {path:'', redirectTo:'/home',pathMatch: 'full'}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -12,13 +12,15 @@ import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { CardCityComponent } from './components/card-city/card-city.component';
+import { LoginComponent } from './components/login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     FooterComponent,
-    CardCityComponent
+    CardCityComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
