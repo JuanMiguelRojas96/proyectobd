4d16cf69a66f0a22b0abf6556ea51b5a4be8f08d
jean heyller palomino <heyller-19@outlook.com>
2023-12-11 21:38:31 -0500

    ruta de las ciudades añadidas con exito

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,10 +1,18 @@
 const { Router } = require('express');
+const postCity = require("../controllers/postCity")
+const getAllCities = require("../controllers/getCity")
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
 
 
 const router = Router();
 
+// ruta para añadir una ciudad
+router.post('/cinefilos/city', postCity);
+
+//ruta para traer las ciudades
+
+router.get('/cinefilos/city',getAllCities);
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
 
